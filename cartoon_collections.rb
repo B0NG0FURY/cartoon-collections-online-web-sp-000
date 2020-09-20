64225cbe88c0e0eab3cdd1aa5a913c9c66baff79
John Troutman <jtroutmanman@gmail.com>
2020-09-20 03:57:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,10 @@ def long_planeteer_calls(array)
   long_words.include?(true)
 end
 
-def find_the_cheese
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |word|
+    
+  end
 end
